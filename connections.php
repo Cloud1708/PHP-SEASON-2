56ad48ed93ff0eaa5554fc58c6a88a6a5e5c4a95
Cloud1708 <ch512291@gnail.com>
2025-08-13 22:28:49 +0800

    bago

---
+++ connections.php
@@ -0,0 +1,25 @@
+<?php
+
+$connections = mysqli_connect("localhost", "root", "", "my_database");
+    if (mysqli_connect_errno()) {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+?>
+
+<style>
+    .btn-primary{
+        -webkit-border-radius: 0;
+        -moz-border-radius: 0;
+        border-radius: 0px;
+        font-family: Georgia;
+         color: #ffffff;
+        background: #34d9bd;
+        padding: 6px 20px 8px 20px;
+        text-decoration: none;
+    }
+
+    .btn-primary:hover {
+        background: #4ccfb3;
+        text-decoration: none;
+    }
+</style>
